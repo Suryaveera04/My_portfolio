0b4689e83956b0148a6d7a7ef3d1f2e7db185473
Suryaveera04 <suryaveera0427@gmail.com>
2025-10-03 12:54:05 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: "Surya's Portfolio",
+  description: 'Created by surya',
   generator: 'v0.dev',
 }
 
